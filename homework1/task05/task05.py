3231d5d668d1db28c9d98dbce2e8658186c08a3d
vladimir-sashin <sashin.v.a@gmail.com>
2021-03-13 21:36:36 +0300

    Added HW1 task 05

---
+++ homework1/task05/task05.py
@@ -0,0 +1,27 @@
+"""
+Given a list of integers numbers "nums".
+
+You need to find a sub-array with length less equal to "k", with maximal sum.
+
+The written function should return the sum of this sub-array.
+
+Examples:
+    nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
+    result = 16
+"""
+from typing import List
+
+
+def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
+    if k > len(nums):
+        print("Given subarray length > array length")
+        pass
+    else:
+        sums = []
+        k_sum = 0
+        for i in range(len(nums) - (k - 1)):
+            for j in range(k):
+                k_sum += nums[i + j]
+            sums.append(k_sum)
+            k_sum = 0
+        return max(sums)
---
+++ homework1/task05/tests/test_find_maximal_subarray_sum.py
@@ -0,0 +1,36 @@
+from task05 import find_maximal_subarray_sum
+
+
+def test_maximal_subarray_sum_happy_path():
+    nums = [1, 3, -1, -3, 5, 3, 6, 7]
+    k = 3
+    actual_result = find_maximal_subarray_sum(nums, k)
+    assert actual_result == 16
+
+
+def test_maximal_subarray_sum_k_is_equal_to_array_length():
+    nums = [-6, 5, 0, -3, 13, 3, 0]
+    k = 7
+    actual_result = find_maximal_subarray_sum(nums, k)
+    assert actual_result == 12
+
+
+def test_maximal_subarray_sum_k_is_1():
+    nums = [-6, 5, 0, -3, 13, 3, 0]
+    k = 1
+    actual_result = find_maximal_subarray_sum(nums, k)
+    assert actual_result == 13
+
+
+def test_maximal_subarray_sum_k_is_0():
+    nums = [1, 2, 0, -3, 1, 3, 0]
+    k = 0
+    actual_result = find_maximal_subarray_sum(nums, k)
+    assert actual_result == 0
+
+
+def test_maximal_subarray_sum_k_is_greater_than_array_length():
+    nums = [1, 2, 0, -3, 1, 3, 0]
+    k = 8
+    actual_result = find_maximal_subarray_sum(nums, k)
+    assert actual_result is None
