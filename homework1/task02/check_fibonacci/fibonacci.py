5bc536dff44a4a9956022c376a6956475d95bd27
vladimir-sashin <sashin.v.a@gmail.com>
2021-03-13 15:47:25 +0300

    Cleared trash

--- homework1/task02/check_fibonacci/fibonacci.py
+++
@@ -1,9 +0,0 @@
-def fibonacci_sequence(quantity) -> list:
-    a, b = (0, 1)
-    for i in range(quantity):
-        yield a
-        (a, b) = (b, a + b)
-
-
-fibonacci = list(fibonacci_sequence(10))
-print(fibonacci)
--- homework1/task02/task02.py
+++ homework1/task02/task02.py
@@ -24,8 +24,3 @@ def check_fibonacci(data: Sequence[int]) -> bool:
         return True
     else:
         return False
-
-
-seq = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
-print(check_fibonacci(seq))
-print(len(seq))
